87c84b33bf3c07326b3eb1cadc7b7e9c7828750b
MahesaFazrie <mahesa.fazrie06@gmail.com>
2025-03-18 10:41:16 +0700

    reupload (jaringan jelek)

--- MainNRSL.cpp
+++ MainNRSL.cpp
@@ -18,29 +18,25 @@ int main()
 	/* Program */
 	CreateList (&MyList);
 	
-	printf("Menambahkan elemen ke awal dan akhir...\n");
+	
     InsVFirst(&MyList, 10);
     InsVFirst(&MyList, 5);
     InsVLast(&MyList, 20);
     PrintInfo(MyList);//List sudah terbentuk, digunakan untuk memanipulasi elemen
 	
 	
-	printf("Menghapus elemen pertama...\n");
+
     DelVFirst(&MyList, &isi);
-    printf("Elemen yang dihapus: %d\n", isi);
     PrintInfo(MyList);
 
-	printf("Menghapus elemen terakhir...\n");
+
     DelVLast(&MyList, &isi);
-    printf("Elemen yang dihapus: %d\n", isi);
     PrintInfo(MyList);
 
-	printf("Menambahkan elemen kembali...\n");
     InsVLast(&MyList, 30);
     InsVLast(&MyList, 40);
     PrintInfo(MyList);
 
-	printf("Menghapus semua elemen...\n");
     DelAll(&MyList);
     PrintInfo(MyList);
 	//aksi mahasiswa untuk implementasi studi kasus
